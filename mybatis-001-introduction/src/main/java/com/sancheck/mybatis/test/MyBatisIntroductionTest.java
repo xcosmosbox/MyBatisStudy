755828f2a81322e00039fe7cbaea2042e748346a
SanCheck <2162381070@qq.com>
2023-01-28 19:18:14 +1100

    Create MyBatisIntroductionTest.java

---
+++ mybatis-001-introduction/src/main/java/com/sancheck/mybatis/test/MyBatisIntroductionTest.java
@@ -0,0 +1,11 @@
+package com.sancheck.mybatis.test;
+
+/**
+ * @author: fengyuxiang
+ * @ClassName: MyBatisIntroductionTest
+ * @version: 1.0
+ * @description:
+ * @create: 28/1/2023
+ */
+public class MyBatisIntroductionTest {
+}
