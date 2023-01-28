0aecb32dafc0c2933c2b08aacd2e2349b96b504e
SanCheck <2162381070@qq.com>
2023-01-28 21:42:27 +1100

    complete MathServiceTest class

--- junit-test/src/test/java/com/sancheck/junit/service/MathServiceTest.java
+++ junit-test/src/test/java/com/sancheck/junit/service/MathServiceTest.java
@@ -1,5 +1,8 @@
 package com.sancheck.junit.service;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author: fengyuxiang
  * @ClassName: MathServiceTest
@@ -8,4 +11,33 @@ package com.sancheck.junit.service;
  * @create: 28/1/2023
  */
 public class MathServiceTest {
+    @Test
+    public void testSum(){
+        MathService mathService = new MathService();
+
+        // getting actual value
+        int actual = mathService.sum(1,2);
+
+        // getting expected value
+        int expected = 3;
+
+        // add assert to test sum method
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testSub(){
+        MathService mathService = new MathService();
+
+        // getting actual value
+        int actual = mathService.sub(10,5);
+
+        // getting expected value
+        int expected = 5;
+
+        // add assert to test sum method
+        Assert.assertEquals(expected,actual);
+    }
+
+
 }
