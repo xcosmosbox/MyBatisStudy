4ddda32ea28c786017cfbdf718967db021e2ce61
SanCheck <2162381070@qq.com>
2023-01-28 21:33:26 +1100

    using junit to test MathService class

---
+++ junit-test/src/main/java/com/sancheck/junit/service/MathService.java
@@ -0,0 +1,20 @@
+package com.sancheck.junit.service;
+
+/**
+ * @author: fengyuxiang
+ * @ClassName: MathService
+ * @version: 1.0
+ * @description:
+ * @create: 28/1/2023
+ */
+public class MathService {
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+
+}
---
+++ junit-test/src/test/java/com/sancheck/junit/service/MathServiceTest.java
@@ -0,0 +1,11 @@
+package com.sancheck.junit.service;
+
+/**
+ * @author: fengyuxiang
+ * @ClassName: MathServiceTest
+ * @version: 1.0
+ * @description:
+ * @create: 28/1/2023
+ */
+public class MathServiceTest {
+}
