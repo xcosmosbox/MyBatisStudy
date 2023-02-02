3d0310e9f88caf18829e1b59976e71a523e5e796
SanCheck <2162381070@qq.com>
2023-02-02 23:29:53 +1100

    Create AccountServiceImpl.java

---
+++ mybatis-004-web/src/main/java/com/sancheck/bank/service/impl/AccountServiceImpl.java
@@ -0,0 +1,11 @@
+package com.sancheck.bank.service.impl;
+
+/**
+ * @author: fengyuxiang
+ * @ClassName: AccountServiceImpl
+ * @version: 1.0
+ * @description:
+ * @create: 2/2/2023
+ */
+public class AccountServiceImpl {
+}
