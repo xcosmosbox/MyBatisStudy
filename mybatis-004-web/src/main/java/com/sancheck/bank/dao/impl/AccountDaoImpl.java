b3a9156d4d0bbed01bf4f1efa198102cf41d2a79
SanCheck <2162381070@qq.com>
2023-02-02 23:39:17 +1100

    Create AccountDaoImpl.java

---
+++ mybatis-004-web/src/main/java/com/sancheck/bank/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,28 @@
+package com.sancheck.bank.dao.impl;
+
+import com.sancheck.bank.dao.AccountDao;
+import com.sancheck.bank.pojo.Account;
+import com.sancheck.bank.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+
+/**
+ * @author: fengyuxiang
+ * @ClassName: AccountDaoImpl
+ * @version: 1.0
+ * @description:
+ * @create: 2/2/2023
+ */
+public class AccountDaoImpl implements AccountDao {
+    @Override
+    public Account selectByActno(String actno) {
+        SqlSession sqlSession = SqlSessionUtil.openSession();
+        Account account = (Account) sqlSession.selectOne("account.selectByActno",actno);
+        sqlSession.close();
+        return account;
+    }
+
+    @Override
+    public int updateByActno(Account act) {
+        return 0;
+    }
+}
