12d8c7b04c5f0e88b9147ac5e0b216af83907261
SanCheck <2162381070@qq.com>
2023-02-02 23:47:26 +1100

    Update AccountServiceImpl.java

--- mybatis-004-web/src/main/java/com/sancheck/bank/service/impl/AccountServiceImpl.java
+++ mybatis-004-web/src/main/java/com/sancheck/bank/service/impl/AccountServiceImpl.java
@@ -1,5 +1,8 @@
 package com.sancheck.bank.service.impl;
 
+import com.sancheck.bank.dao.AccountDao;
+import com.sancheck.bank.dao.impl.AccountDaoImpl;
+import com.sancheck.bank.pojo.Account;
 import com.sancheck.bank.service.AccountService;
 
 /**
@@ -10,8 +13,13 @@ import com.sancheck.bank.service.AccountService;
  * @create: 2/2/2023
  */
 public class AccountServiceImpl implements AccountService {
+
+    private AccountDao accountDao = new AccountDaoImpl();
     @Override
     public void transfer(String fromActno, String toActno, double money) {
+        Account fromAct = accountDao.selectByActno(fromActno);
+        if (fromAct.getBalance() < money){
 
+        }
     }
 }
