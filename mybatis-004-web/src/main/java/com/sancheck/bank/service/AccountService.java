e025010e63dc22d2b31f2f15e85f487d5ca0fbad
SanCheck <2162381070@qq.com>
2023-02-03 00:25:32 +1100

    Update AccountService.java

--- mybatis-004-web/src/main/java/com/sancheck/bank/service/AccountService.java
+++ mybatis-004-web/src/main/java/com/sancheck/bank/service/AccountService.java
@@ -1,5 +1,8 @@
 package com.sancheck.bank.service;
 
+import com.sancheck.bank.exceptions.MoneyNotEnoughException;
+import com.sancheck.bank.exceptions.TransferException;
+
 public interface AccountService {
-    void transfer(String fromActno, String toActno, double money);
+    void transfer(String fromActno, String toActno, double money) throws MoneyNotEnoughException, TransferException;
 }
