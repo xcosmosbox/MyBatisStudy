ae18f20161ad5d74bffd50d18149b984aaf542a3
SanCheck <2162381070@qq.com>
2023-02-04 17:21:39 +1100

    test the select sql statements

--- mybatis-007-param/src/main/java/com/sancheck/mybatis/mapper/StudentMapper.java
+++ mybatis-007-param/src/main/java/com/sancheck/mybatis/mapper/StudentMapper.java
@@ -1,4 +1,20 @@
 package com.sancheck.mybatis.mapper;
 
+import com.sancheck.mybatis.pojo.Student;
+
+import java.util.Date;
+import java.util.List;
+
 public interface StudentMapper {
+
+    List<Student> selectById(Long id);
+    List<Student> selectByName(String name);
+    List<Student> selectByBirth(Date birth);
+    List<Student> selectBySex(Character sex);
+
+
+
+
+
+
 }
