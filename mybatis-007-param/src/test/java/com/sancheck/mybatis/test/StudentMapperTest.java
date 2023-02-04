ef0dfaa7f1c64d2658c07b21d648f9666a83c6b7
SanCheck <2162381070@qq.com>
2023-02-04 18:12:10 +1100

    add testSelectByNameAndSex() function text

--- mybatis-007-param/src/test/java/com/sancheck/mybatis/test/StudentMapperTest.java
+++ mybatis-007-param/src/test/java/com/sancheck/mybatis/test/StudentMapperTest.java
@@ -6,6 +6,8 @@ import com.sancheck.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,6 +20,64 @@ import java.util.List;
 public class StudentMapperTest {
 
     @Test
+    public void testSelectByNameAndSex2(){
+        SqlSession sqlSession = SqlSessionUtil.openSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        List<Student> students = mapper.selectByNameAndSex2("张三",'男');
+        students.forEach(student -> {
+            System.out.println(student);
+        });
+        SqlSessionUtil.close(sqlSession);
+    }
+
+    @Test
+    public void testSelectByNameAndSex(){
+        SqlSession sqlSession = SqlSessionUtil.openSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        List<Student> students = mapper.selectByNameAndSex("张三",'男');
+        students.forEach(student -> {
+            System.out.println(student);
+        });
+        SqlSessionUtil.close(sqlSession);
+    }
+
+    @Test
+    public void testSelectBySex(){
+        SqlSession sqlSession = SqlSessionUtil.openSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        Character sex = Character.valueOf('男');
+        List<Student> students = mapper.selectBySex(sex);
+        students.forEach(student -> {
+            System.out.println(student);
+        });
+        SqlSessionUtil.close(sqlSession);
+    }
+
+    @Test
+    public void testSelectByBirth() throws Exception{
+        SqlSession sqlSession = SqlSessionUtil.openSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date birth = simpleDateFormat.parse("1980-10-11");
+        List<Student> students = mapper.selectByBirth(birth);
+        students.forEach(student -> {
+            System.out.println(student);
+        });
+        SqlSessionUtil.close(sqlSession);
+    }
+
+    @Test
+    public void testSelectByName(){
+        SqlSession sqlSession = SqlSessionUtil.openSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        List<Student> students = mapper.selectByName("李四");
+        students.forEach(student -> {
+            System.out.println(student);
+        });
+        SqlSessionUtil.close(sqlSession);
+    }
+
+    @Test
     public void testSelectById(){
         SqlSession sqlSession = SqlSessionUtil.openSession();
         StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
@@ -25,7 +85,7 @@ public class StudentMapperTest {
         students.forEach(student -> {
             System.out.println(student);
         });
-        sqlSession.close();
+        SqlSessionUtil.close(sqlSession);
     }
 
 
