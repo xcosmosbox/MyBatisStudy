5a3a69f4753a7b8ce8a22be8591e24257f68088c
SanCheck <2162381070@qq.com>
2023-02-04 18:12:26 +1100

    add List<Student> selectByNameAndSex2(@Param("name") String name, @Param("sex") Character sex);

--- mybatis-007-param/src/main/java/com/sancheck/mybatis/mapper/StudentMapper.java
+++ mybatis-007-param/src/main/java/com/sancheck/mybatis/mapper/StudentMapper.java
@@ -1,11 +1,17 @@
 package com.sancheck.mybatis.mapper;
 
 import com.sancheck.mybatis.pojo.Student;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 import java.util.List;
 
 public interface StudentMapper {
+
+    List<Student> selectByNameAndSex2(@Param("name") String name, @Param("sex") Character sex);
+
+    List<Student> selectByNameAndSex(String name, Character sex);
+
 
     List<Student> selectById(Long id);
     List<Student> selectByName(String name);
